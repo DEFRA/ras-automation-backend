a1031d230f43cf10244d3224ad35b4edee638fd2
eswardev nekkanti <eswardev.nekkanti@cognizant.com>
2025-02-07 22:22:53 +0000

    added changes for sharepoint integration

---
+++ src/api/processQueue/config/imisheader.js
@@ -0,0 +1,885 @@
+export const IMISHeaderConfig = [
+  {
+    header: 'LIMT - Core',
+    key: 'limtCore',
+    width: 20,
+    color: '0000FF',
+    note: `test`
+  },
+  {
+    header: 'CPH - Core',
+    key: 'CPHCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'SBI - Core',
+    key: 'SBICore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'FRN - Core',
+    key: 'FRNCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Scheme - Core',
+    key: 'schemeCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Scheme Year - Core',
+    key: 'schemeYearCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Selection method - Core',
+    key: 'selectionMethodCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Date notified of inspection - Core',
+    key: 'inspectionDate',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Date received in LIMT pre inspection - Core',
+    key: 'preInspectionDate',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Customer Name - Core',
+    key: 'customerNameCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Address - Core',
+    key: 'addressCore1',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Address - Core',
+    key: 'addressCore2',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Address - Core',
+    key: 'addressCore3',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Address - Core',
+    key: 'addressCore4',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Postcode - Core',
+    key: 'postCodeCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Phone no - Core',
+    key: 'phoneNoCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Mobile - Core',
+    key: 'mobileCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Grid ref - Core',
+    key: 'gridRefCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Customer Name  (If Diff) - Core',
+    key: 'customerNameCore1',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Address (If diff) - Core',
+    key: 'addressDiffCore2',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Address (If diff) - Core',
+    key: 'addressDiffCore3',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Address (If diff) - Core',
+    key: 'addressDiffCore4',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Address (If diff) - Core',
+    key: 'addressDiffCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Number of animals to be inspected/records checked - Core',
+    key: 'animalsInspected',
+    width: 30,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'SPS/RDP area to be inspected - Core',
+    key: 'SPSRDPInspected',
+    width: 30,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Deadline date - Core',
+    key: 'deadlineDateCore',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Latest start by date (RDP only) - Core',
+    key: 'latestStartDateRDP',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Earliest start by date (RDP only) - Core',
+    key: 'earliestStartDateRDP',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Finish On Farm date - Core',
+    key: 'finishFarmDate',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Agreement ref - Core',
+    key: 'agreementRefCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'RITA task no - Core',
+    key: 'RITATaskCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'High value - Core',
+    key: 'highValueCore',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'CSP - Core',
+    key: 'cspCore',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'CSP reason - Core',
+    key: 'cspReasonCore',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Overpayment - Core',
+    key: 'overPaymentCore',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Penalty - Core',
+    key: 'penaltyCore',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Repeat inspection - Core',
+    key: 'repeatInspectionCore',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'DEFRA threat register - Core',
+    key: 'defraThreatRegisterCore',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'DEFRA threat register - Core',
+    key: 'defraThreatRegisterCore',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'SPS claimant - Core',
+    key: 'SPSClaimantCore',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Claim value - Dossier',
+    key: 'claimValueDossier',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Date EA info requested - Dossier',
+    key: 'EADateInfoDossier',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'RS QC inspection - Dossier',
+    key: 'RSQCInspectionDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Date EA info received - Dossier',
+    key: 'DateEAReceivedDossier',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Selected for standalone XC - Dossier',
+    key: 'standaloneXCDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Soil protection report - Dossier',
+    key: 'soilProtectionReportDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'TB inspection due date? - Dossier',
+    key: 'TBInspectionDueDateDossier',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Dairy farmer - Dossier',
+    key: 'dairyFarmerDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Also selected for EA or AH - Dossier',
+    key: 'selectedEAAHDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'UTP piggyback - Dossier',
+    key: 'UTPPiggybackDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Recommended date - Dossier',
+    key: 'recommendedDateDossier',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Pig keeper - Dossier',
+    key: 'pigKeeperDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'XC report WFA - Dossier',
+    key: 'XCReportWFADossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'TB inspection last completed? - Dossier',
+    key: 'TBInspectionCompletedDossier',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Selected For MQ inspection - Dossier',
+    key: 'selectedMQInspectionDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Protein crops - Dossier',
+    key: 'proteinCropDossier',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'English cross border - Dossier',
+    key: 'englishCrossBorderDossier',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Number of Subsidiary Holdings - Dossier',
+    key: 'SubsidiaryHoldingsDossier',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Vendor number - RDP',
+    key: 'vendorNumberRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Risk - RDP',
+    key: 'riskRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'NE region - RDP',
+    key: 'NERegionRDP',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Number of agreements - RDP',
+    key: 'NoOfAgreementsRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Agreement amended - RDP',
+    key: 'agreementAmendedRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Total fields - RDP',
+    key: 'totalFieldsRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Agreement start - RDP',
+    key: 'agreementStartRDP',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Agreement end - RDP',
+    key: 'agreementEndRDP',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Previous inspection result - RDP',
+    key: 'previousInspectionResultRDP',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Agreement years - RDP',
+    key: 'agreementYearsRDP',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Rotational options - RDP',
+    key: 'rotationalOptionsRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Common land - RDP',
+    key: 'commonLandRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Article 13 - RDP',
+    key: 'article13RDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'GFP - RDP',
+    key: 'GFPRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Capital works - RDP',
+    key: 'capitalWorksRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Population - RDP',
+    key: 'populationRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Start on farm date - RDP',
+    key: 'startFarmDateRDP',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Total ELS points - RDP',
+    key: 'totalELSPointsRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'ELS Points threshold - RDP',
+    key: 'ELSPointsThresholdRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Total OELS points - RDP',
+    key: 'totalOELSPointsRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Points threshold - RDP',
+    key: 'pointsThresholdRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Within 3% of ELS points - RDP',
+    key: 'ELSPointsRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'ESA target region - RDP',
+    key: 'ESATargetRegionRDP',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'ESA IV - RDP',
+    key: 'ESAIVRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: '1:5000 GFP/SPS maps required? - RDP',
+    key: 'mapsRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'SPS Claim form required? - RDP',
+    key: 'spsClaimFormRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'XCOM - RDP',
+    key: 'XCOMRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'Conservation plan - RDP',
+    key: 'conservationPlanRDP',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'End Year - RDP',
+    key: 'endYearRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Lifetime Value - RDP',
+    key: 'lifetimeValueRDP',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Selection basis - RDP',
+    key: 'selectionBasisRDP',
+    width: 25,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Inspection ref - SAG',
+    key: 'inspectionRefSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Origin of AI data - SAG',
+    key: 'originAIDataSAG',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Number of sheep ONTO holding - SAG',
+    key: 'sheepONTOHoldingSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Form sent to selected CPH - SAG',
+    key: 'selectedCPHSAG',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'SPS claimant in previous year - SAG',
+    key: 'SPSClaimantSAG',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header:
+      'Number of common land links requested (see Common land links) - SAG',
+    key: 'commonLandLinksSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Total sheep on commons - SAG',
+    key: 'sheepCommonsSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Date of inspection in previous 1 year - SAG',
+    key: 'inspectionPreviousYearSAG',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Sheep & goat inventory on selected CPH (previous AI data) - SAG',
+    key: 'sheepGoatInventoryCPHSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Sheep on inspection location (latest AI data) - SAG',
+    key: 'sheepInspectionLocationSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Total sheep on associated holdings - SAG',
+    key: 'totalSheepHoldingsSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Premises type - SAG',
+    key: 'premisesTypeSAG',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Date notified of AI information  - SAG',
+    key: 'dateNotifiedAIInfoSAG',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'Number of sheep OFF holding - SAG',
+    key: 'numberSheepOffHoldingSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Number of goats OFF holding - SAG',
+    key: 'goatsOffHoldingSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Number of batch movements - SAG',
+    key: 'NoofBatchMovementsSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Form received from selected CPH - SAG',
+    key: 'formReceivedSelectedCPHSAG',
+    width: 20,
+    color: 'FFA500',
+    note: 'test'
+  },
+  {
+    header: 'SPS claim value in previous year - SAG',
+    key: 'SPSClainPreviousYearSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Total goats on commons - SAG',
+    key: 'totalGoatsCommonSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Total commons used - SAG',
+    key: 'totalCommonsUsedSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Sheep inventory on selected CPH (previous AI data) - SAG',
+    key: 'sheepInventorySelectedCPHSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Goats on inspection location (latest AI data) - SAG',
+    key: 'goatsInspectionLocationSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Total goats on associated holdings - SAG',
+    key: 'totalGoatsAssociatedHoldingsSAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Number of lines/CPHs on selected AI - SAG',
+    key: 'noOfLinesSelectedAISAG',
+    width: 20,
+    color: '40E0D0',
+    note: 'test'
+  },
+  {
+    header: 'Date in Siti Agri - Dossier',
+    key: 'dateSitiAgriDossier',
+    width: 20,
+    color: 'FFFF00',
+    note: 'test'
+  },
+  {
+    header: 'EA Info Applicable - Dossier',
+    key: 'EAInfoAppDossier',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'Optimum Timing date - RDP',
+    key: 'optimumTimingDateRDP',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  },
+  {
+    header: 'LIMT notes',
+    key: 'LIMTNotes',
+    width: 20,
+    color: '0000FF',
+    note: 'test'
+  }
+]
+
+export const headerNotes = {
+  'LIMT - Core': 'test'
+}
+
+// Define headers for first Tab
+export const headersFirstTab = [
+  {
+    header: 'Input constraints',
+    key: 'id',
+    width: 1000
+  }
+]
+
+// Define colors for different rows
+export const rowColors = [
+  { color: 'FFFF0000', value: 'Yellow indicates DATE' },
+  { color: 'FF00FF00', value: 'Blue indicates TEXT' },
+  { color: 'FF0000FF', value: 'Orange indicates Y or N' },
+  { color: 'FFFFFF00', value: 'Turquoise indicates NUMERIC' },
+  { color: 'FFFFA500', value: 'Purple indicates TIME as HH or HH:MM' },
+  { value: '' },
+  { value: '' },
+  {
+    value:
+      '1. Do not insert columns or re-name headings within the Inspections tab.'
+  },
+  {
+    value:
+      '2. Do not re-name or delete the tabs at the bottom of this document as it will make the upload fail.'
+  },
+  {
+    value:
+      '3. Refer to the cell notes behind the top row for guidance relating to formatting and data entry.'
+  },
+  {
+    value:
+      '4. Use columns DO onwards to record other information, however this will not be loaded into IMIS unless workarounds are used to recycle redundant fields. Liaise with Design if this information is required.'
+  },
+  {
+    value: '5. CPH or SBI or FRN and Names & addresses must be provided.'
+  },
+  {
+    value:
+      '6. Row 2 has been used to indicate the scheme type, however these comments must be removed prior to upload.'
+  },
+  {
+    value:
+      '7. Cells can be hidden if they do not relate to specific schemes, however they must be unhidden before emailing to the ICDT Inspectorate Master List Team.'
+  },
+  {
+    value:
+      '8. Headings may refer to SPS, however these may still required for BPS.'
+  },
+  {
+    value:
+      '9. Refer to CPH areas covered by SEO Groups which can be found on the Guidance Hub at Operations > Compliance & Guidance > IMIS > Related docs'
+  },
+  {
+    value:
+      '10. Once complete email the template to the ICDT Inspectorate Master List shared mailbox at SM - RPA - ISU Inspectorate Master Lists via'
+  }
+]
