ba52e315cec5705034ce19601485398c152ace3e
eswardev nekkanti <eswardev.nekkanti@cognizant.com>
2025-02-27 17:07:20 +0000

    dev testing

--- src/api/processQueue/config/awsConfig.js
+++ src/api/processQueue/config/awsConfig.js
@@ -1,11 +1,13 @@
 import { SQSClient } from '@aws-sdk/client-sqs'
 import { config } from '~/src/config/index.js'
+import { fromNodeProviderChain } from '@aws-sdk/credential-providers'
 
 const awsRegion = config.get('awsRegion')
 const sqsEndpoint =
   'https://sqs.eu-west-2.amazonaws.com/332499610595/ras_automation_backend'
 
 export const sqsClient = new SQSClient({
+  credentials: fromNodeProviderChain(),
   region: awsRegion,
   endpoint: sqsEndpoint
 })
